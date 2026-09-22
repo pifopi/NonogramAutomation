0d1eb48cc84e207089bca8fe56eecf23fc75d5bf
DOTTEL Gaël <dottel.gael@gmail.com>
2026-09-23 01:42:01 +0200

    Fix comment

--- config/Others.lua
+++ config/Others.lua
@@ -1,5 +1,5 @@
 -- For adding nonograms that reward more than 3000 XP use "Module:Data/XP_nonograms_color".
--- If a name uses double quotes use a forward slash (e.g. "The "Z" #68990" -> "The \"Z\" letter").
+-- If a name uses double quotes use a forward slash (e.g. "The "Z" letter" -> "The \"Z\" letter").
 		
 -------------------------- ONLY BASE XP FROM NONOGRAMS! -------------------------- 
 ----------- (Without any bonuses, like Sushi, Date cake, skills, etc.) -----------
